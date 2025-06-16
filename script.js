548979df57b0a94beecb73463710834c2477da1a
enaile2007 <enaile.franca@escola.pr.gov.br>
2025-06-16 15:09:41 +0000

    seja feliz

--- script.js
+++ script.js
@@ -10,9 +10,7 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Realização pessoal.",
-                afirmacao: "Afirmação: Você busca satisfação interna e crescimento constante na sua carreira, e isso é o que mais te impulsiona no dia a dia.
-
- "
+                afirmacao: "Afirmação: Você busca satisfação interna e crescimento constante na sua carreira, e isso é o que mais te impulsiona no dia a dia."
             },
             {
                 texto: " Estabilidade financeira.",
@@ -24,10 +22,8 @@ const perguntas = [
         enunciado: "Como você se vê em 5 anos?",
         alternativas: [
             {
-                texto: "Em um cargo de liderança."
-
-",
-                afirmacao: " Você tem ambições de alcançar uma posição estratégica e influente, onde suas decisões impactem diretamente a empresa."
+                texto: "Em um cargo de liderança.",
+                afirmacao: "Você tem ambições de alcançar uma posição estratégica e influente, onde suas decisões impactem diretamente a empresa."
             },
             {
                 texto: "Com mais equilíbrio entre vida pessoal e profissional?",
@@ -39,38 +35,38 @@ const perguntas = [
         enunciado: "Como você prefere trabalhar?",
         alternativas: [
             {
-                texto:"Em equipe."
-                afirmacao: "Você acredita que o trabalho colaborativo traz mais ideias e soluções criativas, além de um ambiente mais dinâmico e engajador.."
+                texto:"Em equipe.",
+                afirmacao: "Você acredita que o trabalho colaborativo traz mais ideias e soluções criativas, além de um ambiente mais dinâmico e engajador."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "De forma autônoma.",
+                afirmacao: "Sua produtividade aumenta quando tem autonomia para gerenciar seu tempo e suas tarefas, buscando sempre eficiência sem muitas interferências externas."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você se sente valorizado no seu trabalho?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: " Sim, sempre recebo feedback positivo.",
+                afirmacao: "O reconhecimento das suas conquistas é importante para você, e isso faz com que se sinta motivado a continuar se esforçando cada vez mais."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Não, sinto que meu esforço é ignorado.",
+                afirmacao: " A falta de valorização pode ser um desafio para você, o que às vezes leva a um desânimo e até à busca por outras oportunidades de reconhecimento."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Como você lida com desafios profissionais?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto:  "Enfrento-os de frente e busco soluções criativas.",
+                afirmacao: "Você vê os desafios como oportunidades para crescer e aprender, e está sempre disposto a sair da zona de conforto para encontrar novas soluções."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Fico frustrado e, às vezes, desmotivado.",
+                afirmacao: "Desafios podem afetar seu estado emocional e, em momentos de dificuldade, você precisa de mais apoio para se sentir capaz de superá-los."
             }
         ]
     },
